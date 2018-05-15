e905e45bf870d1d73b40449d422998fe16c0daf9
Mike Arias <michael.arias.93@gmail.com>
2018-05-15 17:22:44 +0200

    .

--- plugins/slack_notification_plugin.py
+++ plugins/slack_notification_plugin.py
@@ -69,7 +69,7 @@ elif data['why'] == "api":
     why = " ; triggered by api "
 
 # capture BASE_URL from test file
-with open('../android_simple.py') as f:
+with open('../test-cases/android_simple.py') as f:
     for line in f:
         if 'BASE_URL = ' in line:
             exec(line)
